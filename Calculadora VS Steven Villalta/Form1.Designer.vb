a16442ab97f202adc601a2a91769119f596a07be
50370 <50370@LAPTOP-0J8IULG9>
2020-08-01 23:31:58 -0600

    Calculadora ComoBox

--- Calculadora VS Steven Villalta/Form1.Designer.vb
+++ Calculadora VS Steven Villalta/Form1.Designer.vb
@@ -28,7 +28,14 @@ Partial Class Form1
         Me.txtnum2 = New System.Windows.Forms.TextBox()
         Me.lblrespuesta = New System.Windows.Forms.Label()
         Me.btncalcular = New System.Windows.Forms.Button()
-        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.optsuma = New System.Windows.Forms.RadioButton()
+        Me.optresta = New System.Windows.Forms.RadioButton()
+        Me.optmultiplicacion = New System.Windows.Forms.RadioButton()
+        Me.optdivision = New System.Windows.Forms.RadioButton()
+        Me.optexponente = New System.Windows.Forms.RadioButton()
+        Me.optporcentaje = New System.Windows.Forms.RadioButton()
+        Me.optmod = New System.Windows.Forms.RadioButton()
+        Me.cboOperaciones = New System.Windows.Forms.ComboBox()
         Me.SuspendLayout()
         '
         'lblnum1
@@ -81,23 +88,106 @@ Partial Class Form1
         Me.btncalcular.Text = "Calcular"
         Me.btncalcular.UseVisualStyleBackColor = True
         '
-        'RadioButton1
+        'optsuma
         '
-        Me.RadioButton1.AutoSize = True
-        Me.RadioButton1.Location = New System.Drawing.Point(85, 128)
-        Me.RadioButton1.Name = "RadioButton1"
-        Me.RadioButton1.Size = New System.Drawing.Size(115, 21)
-        Me.RadioButton1.TabIndex = 6
-        Me.RadioButton1.TabStop = True
-        Me.RadioButton1.Text = "RadioButton1"
-        Me.RadioButton1.UseVisualStyleBackColor = True
+        Me.optsuma.AutoSize = True
+        Me.optsuma.Location = New System.Drawing.Point(85, 139)
+        Me.optsuma.Name = "optsuma"
+        Me.optsuma.Size = New System.Drawing.Size(37, 21)
+        Me.optsuma.TabIndex = 6
+        Me.optsuma.TabStop = True
+        Me.optsuma.Text = "+"
+        Me.optsuma.UseVisualStyleBackColor = True
+        '
+        'optresta
+        '
+        Me.optresta.AutoSize = True
+        Me.optresta.Location = New System.Drawing.Point(85, 166)
+        Me.optresta.Name = "optresta"
+        Me.optresta.Size = New System.Drawing.Size(34, 21)
+        Me.optresta.TabIndex = 7
+        Me.optresta.TabStop = True
+        Me.optresta.Text = "-"
+        Me.optresta.UseVisualStyleBackColor = True
+        '
+        'optmultiplicacion
+        '
+        Me.optmultiplicacion.AutoSize = True
+        Me.optmultiplicacion.Location = New System.Drawing.Point(85, 193)
+        Me.optmultiplicacion.Name = "optmultiplicacion"
+        Me.optmultiplicacion.Size = New System.Drawing.Size(34, 21)
+        Me.optmultiplicacion.TabIndex = 8
+        Me.optmultiplicacion.TabStop = True
+        Me.optmultiplicacion.Text = "*"
+        Me.optmultiplicacion.UseVisualStyleBackColor = True
+        '
+        'optdivision
+        '
+        Me.optdivision.AutoSize = True
+        Me.optdivision.Location = New System.Drawing.Point(85, 220)
+        Me.optdivision.Name = "optdivision"
+        Me.optdivision.Size = New System.Drawing.Size(33, 21)
+        Me.optdivision.TabIndex = 9
+        Me.optdivision.TabStop = True
+        Me.optdivision.Text = "/"
+        Me.optdivision.UseVisualStyleBackColor = True
+        '
+        'optexponente
+        '
+        Me.optexponente.AutoSize = True
+        Me.optexponente.Location = New System.Drawing.Point(85, 247)
+        Me.optexponente.Name = "optexponente"
+        Me.optexponente.Size = New System.Drawing.Size(36, 21)
+        Me.optexponente.TabIndex = 10
+        Me.optexponente.TabStop = True
+        Me.optexponente.Text = "^"
+        Me.optexponente.UseVisualStyleBackColor = True
+        '
+        'optporcentaje
+        '
+        Me.optporcentaje.AutoSize = True
+        Me.optporcentaje.Location = New System.Drawing.Point(85, 274)
+        Me.optporcentaje.Name = "optporcentaje"
+        Me.optporcentaje.Size = New System.Drawing.Size(41, 21)
+        Me.optporcentaje.TabIndex = 11
+        Me.optporcentaje.TabStop = True
+        Me.optporcentaje.Text = "%"
+        Me.optporcentaje.UseVisualStyleBackColor = True
+        '
+        'optmod
+        '
+        Me.optmod.AutoSize = True
+        Me.optmod.Location = New System.Drawing.Point(85, 301)
+        Me.optmod.Name = "optmod"
+        Me.optmod.Size = New System.Drawing.Size(61, 21)
+        Me.optmod.TabIndex = 12
+        Me.optmod.TabStop = True
+        Me.optmod.Text = "MOD"
+        Me.optmod.UseVisualStyleBackColor = True
+        '
+        'cboOperaciones
+        '
+        Me.cboOperaciones.FormattingEnabled = True
+        Me.cboOperaciones.Items.AddRange(New Object() {"+", "-", "*", "/", "^", "%", "MOD"})
+        Me.cboOperaciones.Location = New System.Drawing.Point(274, 139)
+        Me.cboOperaciones.Name = "cboOperaciones"
+        Me.cboOperaciones.Size = New System.Drawing.Size(131, 24)
+        Me.cboOperaciones.TabIndex = 13
+        Me.cboOperaciones.Text = "elige una opcion"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.cboOperaciones)
+        Me.Controls.Add(Me.optmod)
+        Me.Controls.Add(Me.optporcentaje)
+        Me.Controls.Add(Me.optexponente)
+        Me.Controls.Add(Me.optdivision)
+        Me.Controls.Add(Me.optmultiplicacion)
+        Me.Controls.Add(Me.optresta)
+        Me.Controls.Add(Me.optsuma)
         Me.Controls.Add(Me.btncalcular)
         Me.Controls.Add(Me.lblrespuesta)
         Me.Controls.Add(Me.txtnum2)
@@ -117,5 +207,12 @@ Partial Class Form1
     Friend WithEvents txtnum2 As TextBox
     Friend WithEvents lblrespuesta As Label
     Friend WithEvents btncalcular As Button
-    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents optsuma As RadioButton
+    Friend WithEvents optresta As RadioButton
+    Friend WithEvents optmultiplicacion As RadioButton
+    Friend WithEvents optdivision As RadioButton
+    Friend WithEvents optexponente As RadioButton
+    Friend WithEvents optporcentaje As RadioButton
+    Friend WithEvents optmod As RadioButton
+    Friend WithEvents cboOperaciones As ComboBox
 End Class
