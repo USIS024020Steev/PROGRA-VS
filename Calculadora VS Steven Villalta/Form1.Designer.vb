f19a2b236f45904867a752f0ff8af3b61268a137
50370 <50370@LAPTOP-0J8IULG9>
2020-08-26 00:33:43 -0600

    Ejercicios con estructuras de control, numeros pares e impares, primos y cajero

--- Calculadora VS Steven Villalta/Form1.Designer.vb
+++ Calculadora VS Steven Villalta/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,199 +20,105 @@ Partial Class Form1
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.lblnum1 = New System.Windows.Forms.Label()
-        Me.lblnum2 = New System.Windows.Forms.Label()
-        Me.txtnum1 = New System.Windows.Forms.TextBox()
-        Me.txtnum2 = New System.Windows.Forms.TextBox()
-        Me.lblrespuesta = New System.Windows.Forms.Label()
-        Me.btncalcular = New System.Windows.Forms.Button()
-        Me.optsuma = New System.Windows.Forms.RadioButton()
-        Me.optresta = New System.Windows.Forms.RadioButton()
-        Me.optmultiplicacion = New System.Windows.Forms.RadioButton()
-        Me.optdivision = New System.Windows.Forms.RadioButton()
-        Me.optexponente = New System.Windows.Forms.RadioButton()
-        Me.optporcentaje = New System.Windows.Forms.RadioButton()
-        Me.optmod = New System.Windows.Forms.RadioButton()
-        Me.cboOperaciones = New System.Windows.Forms.ComboBox()
+        Me.btnProcesar = New System.Windows.Forms.Button()
+        Me.lstejercicios = New System.Windows.Forms.ListBox()
+        Me.btnparImpar = New System.Windows.Forms.Button()
+        Me.txtnum = New System.Windows.Forms.TextBox()
+        Me.lblnum = New System.Windows.Forms.Label()
+        Me.btnprimo = New System.Windows.Forms.Button()
+        Me.btnCajero = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'lblnum1
+        'btnProcesar
         '
-        Me.lblnum1.AutoSize = True
-        Me.lblnum1.Location = New System.Drawing.Point(82, 35)
-        Me.lblnum1.Name = "lblnum1"
-        Me.lblnum1.Size = New System.Drawing.Size(43, 17)
-        Me.lblnum1.TabIndex = 0
-        Me.lblnum1.Text = "num1"
+        Me.btnProcesar.Location = New System.Drawing.Point(62, 338)
+        Me.btnProcesar.Name = "btnProcesar"
+        Me.btnProcesar.Size = New System.Drawing.Size(186, 49)
+        Me.btnProcesar.TabIndex = 0
+        Me.btnProcesar.Text = "Pares e Impares"
+        Me.btnProcesar.UseVisualStyleBackColor = True
         '
-        'lblnum2
+        'lstejercicios
         '
-        Me.lblnum2.AutoSize = True
-        Me.lblnum2.Location = New System.Drawing.Point(246, 37)
-        Me.lblnum2.Name = "lblnum2"
-        Me.lblnum2.Size = New System.Drawing.Size(43, 17)
-        Me.lblnum2.TabIndex = 1
-        Me.lblnum2.Text = "num2"
+        Me.lstejercicios.FormattingEnabled = True
+        Me.lstejercicios.ItemHeight = 16
+        Me.lstejercicios.Location = New System.Drawing.Point(62, 43)
+        Me.lstejercicios.Name = "lstejercicios"
+        Me.lstejercicios.Size = New System.Drawing.Size(186, 276)
+        Me.lstejercicios.TabIndex = 1
         '
-        'txtnum1
+        'btnparImpar
         '
-        Me.txtnum1.Location = New System.Drawing.Point(131, 32)
-        Me.txtnum1.Name = "txtnum1"
-        Me.txtnum1.Size = New System.Drawing.Size(100, 22)
-        Me.txtnum1.TabIndex = 2
+        Me.btnparImpar.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnparImpar.Location = New System.Drawing.Point(358, 150)
+        Me.btnparImpar.Name = "btnparImpar"
+        Me.btnparImpar.Size = New System.Drawing.Size(173, 49)
+        Me.btnparImpar.TabIndex = 2
+        Me.btnparImpar.Text = "Par o Impar?"
+        Me.btnparImpar.UseVisualStyleBackColor = True
         '
-        'txtnum2
+        'txtnum
         '
-        Me.txtnum2.Location = New System.Drawing.Point(295, 32)
-        Me.txtnum2.Name = "txtnum2"
-        Me.txtnum2.Size = New System.Drawing.Size(100, 22)
-        Me.txtnum2.TabIndex = 3
+        Me.txtnum.Location = New System.Drawing.Point(499, 88)
+        Me.txtnum.Name = "txtnum"
+        Me.txtnum.Size = New System.Drawing.Size(100, 22)
+        Me.txtnum.TabIndex = 3
         '
-        'lblrespuesta
+        'lblnum
         '
-        Me.lblrespuesta.AutoSize = True
-        Me.lblrespuesta.Location = New System.Drawing.Point(445, 37)
-        Me.lblrespuesta.Name = "lblrespuesta"
-        Me.lblrespuesta.Size = New System.Drawing.Size(83, 17)
-        Me.lblrespuesta.TabIndex = 4
-        Me.lblrespuesta.Text = "respuesta ?"
+        Me.lblnum.AutoSize = True
+        Me.lblnum.Location = New System.Drawing.Point(417, 91)
+        Me.lblnum.Name = "lblnum"
+        Me.lblnum.Size = New System.Drawing.Size(62, 17)
+        Me.lblnum.TabIndex = 4
+        Me.lblnum.Text = "Numero:"
         '
-        'btncalcular
+        'btnprimo
         '
-        Me.btncalcular.Location = New System.Drawing.Point(228, 78)
-        Me.btncalcular.Name = "btncalcular"
-        Me.btncalcular.Size = New System.Drawing.Size(75, 23)
-        Me.btncalcular.TabIndex = 5
-        Me.btncalcular.Text = "Calcular"
-        Me.btncalcular.UseVisualStyleBackColor = True
+        Me.btnprimo.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnprimo.Location = New System.Drawing.Point(558, 150)
+        Me.btnprimo.Name = "btnprimo"
+        Me.btnprimo.Size = New System.Drawing.Size(173, 49)
+        Me.btnprimo.TabIndex = 5
+        Me.btnprimo.Text = "Primo?"
+        Me.btnprimo.UseVisualStyleBackColor = True
         '
-        'optsuma
+        'btnCajero
         '
-        Me.optsuma.AutoSize = True
-        Me.optsuma.Location = New System.Drawing.Point(85, 139)
-        Me.optsuma.Name = "optsuma"
-        Me.optsuma.Size = New System.Drawing.Size(37, 21)
-        Me.optsuma.TabIndex = 6
-        Me.optsuma.TabStop = True
-        Me.optsuma.Text = "+"
-        Me.optsuma.UseVisualStyleBackColor = True
-        '
-        'optresta
-        '
-        Me.optresta.AutoSize = True
-        Me.optresta.Location = New System.Drawing.Point(85, 166)
-        Me.optresta.Name = "optresta"
-        Me.optresta.Size = New System.Drawing.Size(34, 21)
-        Me.optresta.TabIndex = 7
-        Me.optresta.TabStop = True
-        Me.optresta.Text = "-"
-        Me.optresta.UseVisualStyleBackColor = True
-        '
-        'optmultiplicacion
-        '
-        Me.optmultiplicacion.AutoSize = True
-        Me.optmultiplicacion.Location = New System.Drawing.Point(85, 193)
-        Me.optmultiplicacion.Name = "optmultiplicacion"
-        Me.optmultiplicacion.Size = New System.Drawing.Size(34, 21)
-        Me.optmultiplicacion.TabIndex = 8
-        Me.optmultiplicacion.TabStop = True
-        Me.optmultiplicacion.Text = "*"
-        Me.optmultiplicacion.UseVisualStyleBackColor = True
-        '
-        'optdivision
-        '
-        Me.optdivision.AutoSize = True
-        Me.optdivision.Location = New System.Drawing.Point(85, 220)
-        Me.optdivision.Name = "optdivision"
-        Me.optdivision.Size = New System.Drawing.Size(33, 21)
-        Me.optdivision.TabIndex = 9
-        Me.optdivision.TabStop = True
-        Me.optdivision.Text = "/"
-        Me.optdivision.UseVisualStyleBackColor = True
-        '
-        'optexponente
-        '
-        Me.optexponente.AutoSize = True
-        Me.optexponente.Location = New System.Drawing.Point(85, 247)
-        Me.optexponente.Name = "optexponente"
-        Me.optexponente.Size = New System.Drawing.Size(36, 21)
-        Me.optexponente.TabIndex = 10
-        Me.optexponente.TabStop = True
-        Me.optexponente.Text = "^"
-        Me.optexponente.UseVisualStyleBackColor = True
-        '
-        'optporcentaje
-        '
-        Me.optporcentaje.AutoSize = True
-        Me.optporcentaje.Location = New System.Drawing.Point(85, 274)
-        Me.optporcentaje.Name = "optporcentaje"
-        Me.optporcentaje.Size = New System.Drawing.Size(41, 21)
-        Me.optporcentaje.TabIndex = 11
-        Me.optporcentaje.TabStop = True
-        Me.optporcentaje.Text = "%"
-        Me.optporcentaje.UseVisualStyleBackColor = True
-        '
-        'optmod
-        '
-        Me.optmod.AutoSize = True
-        Me.optmod.Location = New System.Drawing.Point(85, 301)
-        Me.optmod.Name = "optmod"
-        Me.optmod.Size = New System.Drawing.Size(61, 21)
-        Me.optmod.TabIndex = 12
-        Me.optmod.TabStop = True
-        Me.optmod.Text = "MOD"
-        Me.optmod.UseVisualStyleBackColor = True
-        '
-        'cboOperaciones
-        '
-        Me.cboOperaciones.FormattingEnabled = True
-        Me.cboOperaciones.Items.AddRange(New Object() {"+", "-", "*", "/", "^", "%", "MOD"})
-        Me.cboOperaciones.Location = New System.Drawing.Point(274, 139)
-        Me.cboOperaciones.Name = "cboOperaciones"
-        Me.cboOperaciones.Size = New System.Drawing.Size(131, 24)
-        Me.cboOperaciones.TabIndex = 13
-        Me.cboOperaciones.Text = "elige una opcion"
+        Me.btnCajero.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCajero.Location = New System.Drawing.Point(485, 214)
+        Me.btnCajero.Name = "btnCajero"
+        Me.btnCajero.Size = New System.Drawing.Size(126, 66)
+        Me.btnCajero.TabIndex = 6
+        Me.btnCajero.Text = "Cajero "
+        Me.btnCajero.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.cboOperaciones)
-        Me.Controls.Add(Me.optmod)
-        Me.Controls.Add(Me.optporcentaje)
-        Me.Controls.Add(Me.optexponente)
-        Me.Controls.Add(Me.optdivision)
-        Me.Controls.Add(Me.optmultiplicacion)
-        Me.Controls.Add(Me.optresta)
-        Me.Controls.Add(Me.optsuma)
-        Me.Controls.Add(Me.btncalcular)
-        Me.Controls.Add(Me.lblrespuesta)
-        Me.Controls.Add(Me.txtnum2)
-        Me.Controls.Add(Me.txtnum1)
-        Me.Controls.Add(Me.lblnum2)
-        Me.Controls.Add(Me.lblnum1)
+        Me.ClientSize = New System.Drawing.Size(1057, 450)
+        Me.Controls.Add(Me.btnCajero)
+        Me.Controls.Add(Me.btnprimo)
+        Me.Controls.Add(Me.lblnum)
+        Me.Controls.Add(Me.txtnum)
+        Me.Controls.Add(Me.btnparImpar)
+        Me.Controls.Add(Me.lstejercicios)
+        Me.Controls.Add(Me.btnProcesar)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Ejercicios con estructuras de control"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents lblnum1 As Label
-    Friend WithEvents lblnum2 As Label
-    Friend WithEvents txtnum1 As TextBox
-    Friend WithEvents txtnum2 As TextBox
-    Friend WithEvents lblrespuesta As Label
-    Friend WithEvents btncalcular As Button
-    Friend WithEvents optsuma As RadioButton
-    Friend WithEvents optresta As RadioButton
-    Friend WithEvents optmultiplicacion As RadioButton
-    Friend WithEvents optdivision As RadioButton
-    Friend WithEvents optexponente As RadioButton
-    Friend WithEvents optporcentaje As RadioButton
-    Friend WithEvents optmod As RadioButton
-    Friend WithEvents cboOperaciones As ComboBox
+    Friend WithEvents btnProcesar As Button
+    Friend WithEvents lstejercicios As ListBox
+    Friend WithEvents btnparImpar As Button
+    Friend WithEvents txtnum As TextBox
+    Friend WithEvents lblnum As Label
+    Friend WithEvents btnprimo As Button
+    Friend WithEvents btnCajero As Button
 End Class
--- Calculadora VS Steven Villalta/Form1.vb
+++ Calculadora VS Steven Villalta/Form1.vb
@@ -1,63 +1,70 @@
 ﻿Public Class Form1
-    Private Sub btncalcular_Click(sender As Object, e As EventArgs) Handles btncalcular.Click
-        'Dim es una palabra reservada para declarar una variable y as es el tipo de variable que sera
-        Dim num1, num2, respuesta As Double 'Declaro las variables en una misma linea al mismo tiempo, en este caso "Double" 
 
-        num1 = txtnum1.Text 'Se captura el valor que digite el usario en la caja de texto numero 1
-        num2 = txtnum2.Text 'Se captura el valor que digite el usario en la caja de texto numero 2
+    Private Sub btnProcesar_Click(sender As Object, e As EventArgs) Handles btnProcesar.Click
+        lstejercicios.Items.Clear()
 
-        If optsuma.Checked Then
-            lblrespuesta.Text = num1 + num2
+        lstejercicios.Items.Add("numeros pares del 1 al 20")
+        For i = 2 To 20 Step 2
+            lstejercicios.Items.Add(i)
+        Next
+
+        lstejercicios.Items.Add("numeros impares del 1 al 20")
+        For i = 1 To 20 Step 2
+            lstejercicios.Items.Add(i)
+        Next
+    End Sub
+
+    Private Sub btnparImpar_Click(sender As Object, e As EventArgs) Handles btnparImpar.Click
+        Dim num = txtnum.Text
+        If num Mod 2 = 0 Then
+            MessageBox.Show("el numero: " + num + "es par")
+
+        Else
+            MessageBox.Show("el numero: " + num + "es impar")
         End If
+    End Sub
 
-        If optresta.Checked Then
-            lblrespuesta.Text = num1 - num2
+
+    Private Sub btnprimo_Click(sender As Object, e As EventArgs) Handles btnprimo.Click
+        Dim num = txtnum.Text
+        Dim i = 0, acu = 1
+        While i < 3 And acu <= num
+            If num Mod acu = 0 Then
+                i += 1  'i = i + 1
+            End If
+            acu += 1
+        End While
+        If i <= 2 Then
+            MessageBox.Show("Es primo")
+        Else
+            MessageBox.Show("NO es primo")
         End If
+    End Sub
 
-        If optmultiplicacion.Checked Then
-            lblrespuesta.Text = num1 * num2
-        End If
+    Private Sub btnCajero_Click(sender As Object, e As EventArgs) Handles btnCajero.Click
+        Dim denominaciones() = {100, 50, 20, 10, 5, 1, 0.5, 0.25, 0.1, 0.05, 0.01}, valor = txtnum.Text, cantidad = 0
+        Dim denomina As String
 
-        If optdivision.Checked Then
-            lblrespuesta.Text = num1 / num2
-        End If
-
-        If optexponente.Checked Then
-            lblrespuesta.Text = num1 ^ num2
-        End If
-
-        If optporcentaje.Checked Then
-            lblrespuesta.Text = num1 * num2 / 100
-        End If
-
-        If optmod.Checked Then
-            lblrespuesta.Text = num1 Mod num2
-        End If
-
-        Select Case cboOperaciones.SelectedIndex
-            Case 0
-                lblrespuesta.Text = num1 + num2
-
-            Case 1
-                lblrespuesta.Text = num1 - num2
-
-            Case 2
-                lblrespuesta.Text = num1 * num2
-
-            Case 3
-                lblrespuesta.Text = num1 / num2
-
-            Case 4
-                lblrespuesta.Text = num1 ^ num2
-
-            Case 5
-                lblrespuesta.Text = num1 * num2 / 100
-
-            Case 6
-                lblrespuesta.Text = num1 Mod num2
-
-        End Select
+        lstejercicios.Items.Clear()
 
 
+        For Each denominacion In denominaciones
+            While valor > 0 And denominacion <= valor
+
+                valor = Math.Round(valor - denominacion, 2)
+                cantidad += 1
+
+            End While
+            If cantidad > 0 Then
+                If denominacion <= 1 Then
+                    denomina = "moneda de"
+                Else
+                    denomina = "billete de "
+
+                End If
+                lstejercicios.Items.Add(cantidad.ToString() + denomina + denominacion.ToString())
+                cantidad = 0
+            End If
+        Next
     End Sub
 End Class
